55c0fd19e15c140dec8bf230c488d6e4f9a5971d
luandevpro <luandevpro@gmail.com>
2019-03-31 10:46:13 +0700

    api signup

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,8 +1,36 @@
+const User = require("../models/user")
+   bcrypt  = require("bcrypt")
 
 exports.home = (req,res) => {
    res.json({message: "Hello World Web API"})
 }
 
 exports.signup = (req,res) => {
-   res.json({message: "signup"})
+   User
+      .findOne({email: req.body.email})
+      .then(user => {
+         if(user){
+            return res.status(400).json({message: "Email đã tồn tại !!!"})
+         }else{
+            const newUser = new User({
+               username: req.body.username,
+               age: req.body.age,
+               email: req.body.email,
+               password: req.body.password,
+            })
+            bcrypt.hash(newUser.password, 10 , function(err,hash){
+               if(err){
+                  return res.json(err)
+               }else{
+                  newUser.password = hash
+                  newUser
+                     .save()
+                     .then(user => {
+                        res.json(user)
+                     })
+                     .catch(err => res.json(err))
+               }
+            })
+         }
+      })
 }
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express       = require("express")
       app           = express()
-      port          = process.env.port || 3000
- 
+      port          = process.env.port || 8080
+      dotEnv        = require("dotenv")
+      mongoose      = require("mongoose")
+
+dotEnv.config()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+mongoose
+   .connect(process.env.URI_DB , { useNewUrlParser: true })
+   .then(() => console.log("connect database success"))
+   .catch(err => console.log(err))
+
 app.use("/",require("./routes"))
+
 app.listen(port, () => console.log(`port`))
